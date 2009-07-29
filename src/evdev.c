ljosa/xf86-input-evdev-spacefn

Only initialize the number of buttons we actually have.
This takes into account driver-configured button mappings, i.e. if device
with one button has this button mapped to 25 through the ButtonMapping
option, the X server will think the device has result 25 buttons.

Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>

src/evdev.c
@@ -1115,9 +1115,7 @@ EvdevAddButtonClass(DeviceIntPtr device)
     pInfo = device->public.devicePrivate;
     pEvdev = pInfo->private;
 
-    /* FIXME: count number of actual buttons */
-    if (!InitButtonClassDeviceStruct(device, ArrayLength(pEvdev->btnmap),
-                                     pEvdev->btnmap))
+    if (!InitButtonClassDeviceStruct(device, pEvdev->num_buttons, pEvdev->btnmap))
         return !Success;
 
     return Success;

phomes/xf86-input-evdev

(cherry picked from commit 255b9f6bbf374a315750019c6fadc5f82fb7d41d)

src/evdev.c
@@ -1095,9 +1095,7 @@ EvdevAddButtonClass(DeviceIntPtr device)
     pInfo = device->public.devicePrivate;
     pEvdev = pInfo->private;
 
-    /* FIXME: count number of actual buttons */
-    if (!InitButtonClassDeviceStruct(device, ArrayLength(pEvdev->btnmap),
-                                     pEvdev->btnmap))
+    if (!InitButtonClassDeviceStruct(device, pEvdev->buttons, pEvdev->btnmap))
         return !Success;
 
     return Success;